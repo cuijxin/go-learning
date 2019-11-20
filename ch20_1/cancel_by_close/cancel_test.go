aeabc902a730a0ebb407211bb32fea8e722c5689
cuijxin <cuijxin@126.com>
2019-11-20 16:47:27 +0800

    cancel_test.go

---
+++ ch20_1/cancel_by_close/cancel_test.go
@@ -0,0 +1,41 @@
+package cancelbyclose
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func isCancelled(cancelChan chan struct{}) bool {
+	select {
+	case <-cancelChan:
+		return true
+	default:
+		return false
+	}
+}
+
+func cancel1(cancelChan chan struct{}) {
+	cancelChan <- struct{}{}
+}
+
+func cancel2(cancelChan chan struct{}) {
+	close(cancelChan)
+}
+
+func TestCancel(t *testing.T) {
+	cancelChan := make(chan struct{}, 0)
+	for i := 0; i < 5; i++ {
+		go func(i int, cancelCh chan struct{}) {
+			for {
+				if isCancelled(cancelCh) {
+					break
+				}
+				time.Sleep(time.Millisecond * 5)
+			}
+			fmt.Println(i, "Cancelled")
+		}(i, cancelChan)
+	}
+	cancel1(cancelChan)
+	time.Sleep(time.Second * 1)
+}
